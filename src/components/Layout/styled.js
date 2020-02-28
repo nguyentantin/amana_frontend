aad22197c5c33d53a15ee7eabccafb3a37529d33
tin.nt <nguyentantin.tb@gmail.com>
2020-02-28 10:19:31 +0700

    Refactor code

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -1,17 +1,5 @@
 import styled from 'styled-components'
 
-export const LayoutContainer = styled.div`
-  background: #fff;
-  flex-grow: 1;
-  overflow: hidden;
-  margin-left: 260px;
-`
-
-export const MainContainer = styled.div`
-  padding: 30px;
-  margin-top: 60px;
-`
-
 export const MasterLayoutWrapper = styled.div`
   ul,
   ol {
@@ -22,19 +10,6 @@ export const MasterLayoutWrapper = styled.div`
     color: #ccc;
     font-weight: 200;
   }
-
-  /* NOTE: Fix ie flex container min-height not work */
-  @media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {
-    display: flex;
-    flex-direction: column;
-  }
-`
-
-export const DashboardLayoutWrapper = styled.div`
-  background: #fff;
-  display: flex;
-  flex-direction: row;
-  min-height: 100vh;
 `
 
 export const LogoWrapper = styled.span`
