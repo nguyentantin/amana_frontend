b97c5ef87a5fce1de8854fd138cb923cf1e5ad7a
tin.nt <nguyentantin.tb@gmail.com>
2020-02-28 15:14:35 +0700

    Add seo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
 import { I18nextProvider } from 'react-i18next'
 import { ThemeProvider } from 'styled-components'
+import { Helmet } from 'react-helmet'
 
 import 'normalize.css'
 import 'toastr/toastr.scss'
@@ -16,6 +17,7 @@ import { configure } from './store'
 import i18n from './i18n'
 import AppRoutes from './components/Application/AppRoutes'
 import route from './routes'
+import logo from './assets/images/App_logo.png'
 
 /**
  * Main Application class.
@@ -39,6 +41,17 @@ class Application {
         <Provider store={store}>
           <ConnectedRouter history={history}>
             <ThemeProvider theme={theme}>
+              <Helmet>
+                <meta name="description" content="Auto build application development by SI1 Studio."/>
+                <meta property="og:title" content="Auto build application development by SI1 Studio."/>
+                <meta property="og:description" content="Auto build application development by SI1 Studio."/>
+                <meta property="og:url" content={window.location}/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:site_name" content="SI1 Build Automation"/>
+                <meta property="og:locale" content="en"/>
+                <meta property="og:image" content={window.location + logo}/>
+                <meta name="twitter:card" content="summary_large_image"/>
+              </Helmet>
               <GlobalStyle/>
               <AppRoutes routes={route}/>
             </ThemeProvider>
