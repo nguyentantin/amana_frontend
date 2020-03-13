25c17595e66a9465385ef697db1a2efb134d00b3
nghti <nghti8020@gmail.com>
2020-03-13 16:28:45 +0700

    Fix

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -36,22 +36,6 @@ class SignInPage extends React.PureComponent {
 
   responseGoogle(response) {
     console.log('response', response)
-    const tokenBlob = new Blob([JSON.stringify({access_token: response.accessToken}, null, 2)], {type : 'application/json'});
-    console.log('tokenBlob', tokenBlob)
-    // const options = {
-    //     method: 'POST',
-    //     body: tokenBlob,
-    //     mode: 'cors',
-    //     cache: 'default'
-    // };
-    // fetch('http://localhost:4000/api/v1/auth/google', options).then(r => {
-    //     const token = r.headers.get('x-auth-token');
-    //     r.json().then(user => {
-    //         if (token) {
-    //             this.setState({isAuthenticated: true, user, token})
-    //         }
-    //     });
-    // })
 }
 
   onFailure(err) {
