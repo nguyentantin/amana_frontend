5e12a4ea6539a26b1faf80625ca4a643beff17d2
tin.nt <nguyentantin.tb@gmail.com>
2020-03-18 15:21:17 +0700

    refactor

--- src/api/Request/ProjectRequest.js
+++ src/api/Request/ProjectRequest.js
@@ -2,7 +2,7 @@ import HttpRequest from '../HttpRequest'
 
 class ProjectRequest extends HttpRequest {
   all(params = {}) {
-    return this.get('/projects', params)
+    return this.get('/ProjectList', params)
   }
 
   detail(projectId) {
