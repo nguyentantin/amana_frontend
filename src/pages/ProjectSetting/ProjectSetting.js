81b803b7579743bf5ec09a3e9cd976d1643c58af
tin.nt <nguyentantin.tb@gmail.com>
2020-03-24 15:34:39 +0700

    Add project setting

--- src/pages/ProjectSetting/ProjectSetting.js
+++ src/pages/ProjectSetting/ProjectSetting.js
@@ -1,6 +1,9 @@
 import React from 'react'
-
 import { Tabs } from 'antd'
+import { StepBackwardOutlined, LeftOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router'
+
 import { Box } from '../../styles/utility'
 import * as Styled from './styled'
 import MemberSetting from './MemberSetting'
@@ -10,9 +13,17 @@ const {TabPane} = Tabs
 
 class ProjectSetting extends React.Component {
   render() {
+    const {match: {params}} = this.props
+
     return (
       <Styled.Container>
         <Box bg="#fff" p={16} borderColor='#fff'>
+          <Box m={3}>
+            <Link to={`/projects/${params.projectId}`}>
+              <LeftOutlined style={{ fontSize: '16px' }}/>
+              Back
+            </Link>
+          </Box>
           <Tabs type="card">
             <TabPane tab='Member Settings' key='1'>
               <MemberSetting/>
@@ -28,4 +39,4 @@ class ProjectSetting extends React.Component {
   }
 }
 
-export default ProjectSetting
+export default withRouter(ProjectSetting)
