f9b797c6bedf2f9e43eb56eae9395be15004a08b
Toantt <unicorn@ubuntu.ubuntu-domain>
2020-03-24 11:09:52 +0700

    add skeleton and avatar project for app build

--- src/pages/Dashboard/ListAppBuild.js
+++ src/pages/Dashboard/ListAppBuild.js
@@ -2,7 +2,7 @@ import React from 'react'
 import moment from 'moment'
 import _ from 'lodash'
 import styled from 'styled-components'
-import { List } from 'antd'
+import { List, Skeleton } from 'antd'
 import { Link } from 'react-router-dom'
 
 import { truncate } from '../../utils/helpers'
@@ -22,22 +22,42 @@ const DescriptionLink = styled(Link)`
     color: rgba(0, 0, 0, 0.45);
 `
 
+const skeletonData = () => {
+  const listData = []
+  for(let i = 0; i < 10; i++) {
+    listData.push({
+      id: i,
+      name: `Placeholder name ${i}`,
+      description: `Placeholder description ${i}`
+    })
+  }
+
+  return listData
+}
+
 const ListAppBuild = (props) => {
   return (
     <ScrollContainer height="800px">
       <h2>Timeline <SmallTitle>Recent builds</SmallTitle></h2>
       <List
         itemLayout="horizontal"
-        dataSource={props.data}
+        dataSource={props.loading ? skeletonData() : props.data}
         renderItem={item => (
-          <List.Item>
-            <List.Item.Meta
-              avatar={<AvatarBox size={55} src={_.get(item, 'project.avatar', null)} name={item.project.name}/>}
-              title={<Link to={`/projects/${item.projectId}/app-build/${item.id}`}><strong># {item.id} { truncate(item.commitChanges, 50) }</strong></Link>}
-              description={<DescriptionLink to={`/projects/${item.projectId}`}>{item.project.name}</DescriptionLink>}
-            />
-            <div><TextMute>{ moment(item.createdAt).fromNow() }</TextMute></div>
-          </List.Item>
+          <React.Fragment>
+            {
+              props.loading
+                ? <Skeleton loading avatar active/>
+                : <List.Item>
+                  <List.Item.Meta
+                    avatar={<AvatarBox size={55} src={_.get(item, 'project.avatar', null)} name={item.project.name}/>}
+                    title={<Link to={`/projects/${item.projectId}/app-build/${item.id}`}><strong># {item.id} { truncate(item.commitChanges, 50) }</strong></Link>}
+                    description={<DescriptionLink to={`/projects/${item.projectId}`}>{item.project.name}</DescriptionLink>}
+                  />
+                  <div><TextMute>{ moment(item.createdAt).fromNow() }</TextMute></div>
+                </List.Item>
+
+            }
+          </React.Fragment>
         )}
       />
     </ScrollContainer>
