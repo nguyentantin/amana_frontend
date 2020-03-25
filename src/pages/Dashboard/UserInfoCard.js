d5085c3e49c6fa9efbe094812d40969eb2de2420
Toantt <unicorn@ubuntu.ubuntu-domain>
2020-03-25 13:26:00 +0700

    save works

---
+++ src/pages/Dashboard/UserInfoCard.js
@@ -0,0 +1,94 @@
+import React from 'react'
+
+import LocalStorage from '../../utils/localStorage'
+import { Card, Col, Dropdown, Menu, Row } from 'antd'
+import { AvatarBox } from '../../components/CoreUI'
+import { Link, withRouter } from 'react-router-dom'
+import styled from 'styled-components'
+import { DownOutlined, LogoutOutlined, MailOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons'
+
+const authInfo = () => {
+  return LocalStorage.getAuthInfo()
+}
+
+const { Meta } = Card
+
+const SmallTitle =  styled.small`
+    font-size: 69%;
+    color: rgba(0, 0, 0, 0.45);
+  `
+
+class UserInfoCard extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.logout = this.logout.bind(this)
+  }
+
+  logout() {
+    const {history} = this.props
+    LocalStorage.removeToken()
+    LocalStorage.removeAuthInfo()
+
+    history.push('/sign-in')
+  }
+
+  dropdownMenu() {
+    return (
+      <Menu>
+        <Menu.Item>
+          <Link to="/"><SettingOutlined/> Profile Settings</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/" onClick={this.logout}><LogoutOutlined/> Log Out</Link>
+        </Menu.Item>
+      </Menu>
+    )
+  }
+
+  render() {
+    return (
+      <Card
+        style={{
+          backgroundColor: 'rgba(240, 242, 245, 1)',
+          marginBottom: 20,
+        }}
+        cover={
+          <AvatarBox
+            size={275}
+            shape="square"
+            style={{
+              backgroundColor: authInfo().color,
+              fontSize: 150,
+            }}
+            name={authInfo().name}
+          />
+        }
+      >
+        <Meta
+          title={
+              <Row justify="space-around" align="middle">
+                <Col span={20}>
+                  <div>
+                    <UserOutlined/>
+                    <span style={{ fontSize: '15px', marginLeft: '5px' }}>{authInfo().name}</span>
+                  </div>
+
+                  <div>
+                    <MailOutlined/>
+                    <SmallTitle style={{ fontSize: '15px', marginLeft: '5px' }}>{authInfo().email}</SmallTitle>
+                  </div>
+                </Col>
+                <Col span={4} style={{ textAlign: 'right' }}>
+                  <Dropdown overlay={this.dropdownMenu()} placement="bottomRight">
+                    <DownOutlined/>
+                  </Dropdown>
+                </Col>
+              </Row>
+          }
+        />
+      </Card>
+    )
+  }
+}
+
+export default withRouter(UserInfoCard)
