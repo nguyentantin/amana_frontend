1a9702d846cc6ff234509758af89cd038137845f
tin.nt <nguyentantin.tb@gmail.com>
2020-03-25 15:47:45 +0700

    Notify error 403

--- src/api/HttpInterceptors.js
+++ src/api/HttpInterceptors.js
@@ -4,6 +4,7 @@ import { isEmpty } from 'lodash'
 
 import history from '../utils/history'
 import LocalStorage from '../utils/localStorage'
+import * as Toastr from '../utils/toastr'
 
 NProgress.configure({ showSpinner: false })
 
@@ -51,6 +52,7 @@ axios.interceptors.response.use(
       case 401:
         break
       case 403:
+        Toastr.error('Access is denied. You may not have the appropriate permissions to access the resource')
         break
       case 404:
         history.push('/404')
