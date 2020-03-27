7920ccb8ad919fd08ac7b9b7ae82643aa2852244
tin.nt <nguyentantin.tb@gmail.com>
2020-03-27 13:52:59 +0700

    add avatar

---
+++ src/assets/avatar/index.js
@@ -0,0 +1,19 @@
+import avatar1 from './1.png'
+import avatar2 from './2.png'
+import avatar3 from './3.png'
+import avatar4 from './4.png'
+import avatar5 from './5.png'
+import avatar6 from './6.png'
+import avatar7 from './7.png'
+import avatar8 from './8.png'
+
+export default [
+  {id: 1, imgSrc: avatar1},
+  {id: 2, imgSrc: avatar2},
+  {id: 3, imgSrc: avatar3},
+  {id: 4, imgSrc: avatar4},
+  {id: 5, imgSrc: avatar5},
+  {id: 6, imgSrc: avatar6},
+  {id: 7, imgSrc: avatar7},
+  {id: 8, imgSrc: avatar8}
+]
