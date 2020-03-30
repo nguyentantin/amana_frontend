cb2e1f2f2993560fd1a6db21c5e7a5fb659b21e7
nghti <nghti8020@gmail.com>
2020-03-30 16:40:51 +0700

    Fix page time-sheets

--- src/pages/TimeSheet/styled.js
+++ src/pages/TimeSheet/styled.js
@@ -1,6 +1,28 @@
 import styled from 'styled-components'
-import { container } from '../../styles/mixins'
+import { Form } from 'antd'
 
-export const Container = styled.div`
-  ${container.centerBox}
+export const StyleForm = styled(Form)`
+  justify-content: flex-end;
+  align-items: center;
+  margin: 15px 0 30px !important;
+  @media screen and (max-width: 600px) {
+    justify-content: flex-start;
+    .ant-form-item {
+      width: 100%;
+      margin-bottom: 15px !important;
+      .ant-picker {
+        display: inherit;
+      }
+    }
+    .ant-btn {
+      margin: 8px auto 0;
+      padding: 0 20%;
+    }
+  }
+`
+
+export const Page = styled.div`
+  .ant-table thead {
+    display: none;
+  }
 `
